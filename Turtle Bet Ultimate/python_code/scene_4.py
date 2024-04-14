c88351ea7cc76de096fcaf9e1000c190ea98bd5a
Kenneth939 <157056087+Kenneth939@users.noreply.github.com>
2024-04-14 21:41:26 +0100

    Update scene_4.py

--- Turtle Bet Ultimate/python_code/scene_4.py
+++ Turtle Bet Ultimate/python_code/scene_4.py
@@ -10,13 +10,17 @@ class Scene4(StandardFrame):
     def __init__(self, window: Tk):
         super().__init__(window)
 
+        #initialization
         self.print_frame()
 
+        #Setup countdown labels
         self.label_3 = Label(self.frame, text="3", bg="white", font=("Courier", 50))
         self.label_2 = Label(self.frame, text="2", bg="white", font=("Courier", 50))
         self.label_1 = Label(self.frame, text="1", bg="white", font=("Courier", 50))
         self.label_go = Label(self.frame, text="GO!", bg="white", font=("Courier", 50))
         self.label_finished = Label(self.frame, text="FINISHED!", bg="white", font=("Courier", 50))
+
+        # Load stand images and create canvases for displaying these images
         upper_stand = PhotoImage(file="../stand_image/upper_stand.png")
         self.upper_stand = upper_stand
         lower_stand = PhotoImage(file="../stand_image/lower_stand.png")
@@ -27,10 +31,13 @@ class Scene4(StandardFrame):
         self.lower_stand_canvas.create_image(0, 0, anchor=NW, image=self.lower_stand)
         self.upper_stand_canvas.place(x=0, y=0)
         self.lower_stand_canvas.place(x=0, y=350)
+
+        #when the sounds play
         self.frame.after(1500, self.play_3_2_1_sound)
         self.frame.after(6000, self.play_ingame_music)
-        for index in range(0, 2):
 
+        # Decide which turtles from the semifinals advance to the final based on player choices and randomness
+        for index in range(0, 2):
             if data.player_choice in data.semi[index]:
                 m = random.randint(0, 1)
                 data.final_turtle_indices[1 - index] = data.semi[1 - index][m]
@@ -40,6 +47,8 @@ class Scene4(StandardFrame):
                 self.turtle_object_semi_list[data.semi[index][1]].turtle.setpos(x=0, y=-25)
                 self.turtle_object_semi_list[data.semi[index][0]].canvas.place(x=0, y=150)
                 self.turtle_object_semi_list[data.semi[index][1]].canvas.place(x=0, y=250)
+
+                # Create visual markers on the turtle race
                 for i in range(0, 7):
                     if i % 2 == 0:
                         self.turtle_object_semi_list[data.semi[index][0]].canvas.create_line(700 + 10 * i, -100,
@@ -48,22 +57,28 @@ class Scene4(StandardFrame):
                         self.turtle_object_semi_list[data.semi[index][1]].canvas.create_line(700 + 10 * i, -100,
                                                                                              700 + 10 * i, 100,
                                                                                              fill="black", width=10)
+
+                # Start the countdown and race for this pair of turtles
                 self.ready_set_go(5, self.turtle_object_semi_list[data.semi[index][0]],
                                   self.turtle_object_semi_list[data.semi[index][1]], index)
 
+    #play the countdown sound 
     def play_3_2_1_sound(self):
         pygame.mixer_music.load("../background_music/3_2_1_go.mp3")
         pygame.mixer_music.play()
 
+    #play the bcakground sound 
     def play_ingame_music(self):
         pygame.mixer_music.load("../background_music/ingame_music.mp3")
         pygame.mixer_music.play()
 
+    #Transition to the next scene after the race is completed
     def change_to_scene_5(self):
         self.frame.destroy()
         new_frame = scene_5.Scene5(self.window).frame
         new_frame.tkraise()
 
+    # Recursively manage the countdown display and initiate turtle movements after "GO!"
     def ready_set_go(self, time, turtle1: StandardTurtleObjectGUI, turtle2: StandardTurtleObjectGUI, i):
         self.frame.after(1000, self.ready_set_go, time - 1, turtle1, turtle2, i)
         if time > 3:
@@ -81,6 +96,8 @@ class Scene4(StandardFrame):
             self.label_go.place(x=350, y=225)
         else:
             self.label_go.destroy()
+            
+            # Race logic to move turtles randomly forward ,determine the winner when one turtle crosses the finish line
             if turtle1.turtle.xcor() < 700 and turtle2.turtle.xcor() < 700:
                 random_move_1 = random.randint(1, 100)
                 random_move_2 = random.randint(1, 100)
