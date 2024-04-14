39abd2bce7d58602a8fac2844617826c6a79e6f9
Kenneth939 <157056087+Kenneth939@users.noreply.github.com>
2024-04-14 21:58:54 +0100

    Update scene_5.py

--- Turtle Bet Ultimate/python_code/scene_5.py
+++ Turtle Bet Ultimate/python_code/scene_5.py
@@ -11,13 +11,17 @@ class Scene5(StandardFrame):
     def __init__(self, window: Tk):
         super().__init__(window)
 
+        #stop the musice
         self.stop_ingame_music()
 
+        #initialization
         self.print_frame()
 
+        #play the background music in a loop after a delay of 3 seconds
         self.frame.after(3000, pygame.mixer_music.load("../background_music/background_music.mp3"))
         pygame.mixer_music.play(loops=-1)
 
+        # Set the direction of all turtles 
         for participant in self.turtle_object_semi_list:
             participant.turtle.setheading(90)
         for participant in self.turtle_object_final_list:
@@ -25,6 +29,7 @@ class Scene5(StandardFrame):
         for participant in self.turtle_object_champion_list:
             participant.turtle.setheading(90)
 
+        # Initialize the tournament tree GUI and assign turtles to their respective places
         self.tournament_tree_GUI = StandardTournamentTreeGUI(self.frame)
         self.tournament_tree_GUI.semi1_turtles[0] = self.turtle_object_semi_list[data.semi[0][0]]
         self.tournament_tree_GUI.semi1_turtles[1] = self.turtle_object_semi_list[data.semi[0][1]]
@@ -33,6 +38,7 @@ class Scene5(StandardFrame):
         self.tournament_tree_GUI.final_turtles[0] = self.turtle_object_final_list[data.final_turtle_indices[0]]
         self.tournament_tree_GUI.final_turtles[1] = self.turtle_object_final_list[data.final_turtle_indices[1]]
 
+         # Update UI based on whether the player's choice is in the final indices
         if data.player_choice not in data.final_turtle_indices:
             self.head_label = Label(self.frame, text="RESULT", bg="green", font=("Courier", 30))
             self.press_label = Label(self.frame, text="(Press any key to continue)", bg="green",
@@ -55,15 +61,18 @@ class Scene5(StandardFrame):
             self.frame.focus_set()
             self.frame.bind("<Key>", self.change_to_scene_6)
 
+    # Fade out to stop the music
     def stop_ingame_music(self):
         pygame.mixer_music.fadeout(1000)
 
+    # Switch to scene 6 and fade out the music
     def change_to_scene_6(self, event):
         self.frame.destroy()
         pygame.mixer_music.fadeout(1000)
         new_frame = scene_6.Scene6(self.window).frame
         new_frame.tkraise()
 
+    # Switch to scene 8 and fade out the music
     def change_to_scene_8(self, event):
         self.frame.destroy()
         pygame.mixer_music.fadeout(1000)
